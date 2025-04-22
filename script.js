29933acd0375b48205d890c98b1d094b2a727350
radicnt <zavierganymede@gmail.com>
2025-04-21 22:10:44 -0400

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// add simple cursor parallax effect
-window.addEventListener("mousemove", e => {
-  document.body.style.setProperty("--x", `${e.clientX}px`);
-  document.body.style.setProperty("--y", `${e.clientY}px`);
-});
